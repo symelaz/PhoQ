0f81d99b9fe198ffd132fd6f0c8a5ffadc90c58f
Symela Lazaridi <94408242+symelaz@users.noreply.github.com>
2024-04-18 15:05:55 +0200

    Update WaterBundle.tcl

--- Figure05_Hydration/WaterBundle.tcl
+++ Figure05_Hydration/WaterBundle.tcl
@@ -1,1 +1,28 @@
 
+#################### Inputs ####################
+# 0 --> psf file of the molecule
+# 1 --> dcd file of the trajectory
+# 2 --> Output file to be written
+
+# Prepare Output file
+set f_r_out [lindex $argv 2]
+puts "Output File to be Written $f_r_out"
+set cutoff 4; #3.5 or 4
+
+# Load Trajectory
+mol new [lindex $argv 0] type psf
+mol addfile [lindex $argv 1] type dcd first 0 last -1 step 1 waitfor all
+
+set nf [molinfo top get numframes]
+
+set outfile [open $f_r_out w]
+for {set i 0} {$i < $nf} {incr i} {
+	puts "frame $i of $nf"
+	set sel1 [atomselect top "water within $cutoff of (protein and resid 32 202 and sidechain)" frame $i]
+	set counter [llength [lsearch -all -exact [$sel1 get type] OT]]
+	set ids [lsort -unique [$sel1 get resid]]
+	puts $outfile "$i,$counter,$ids"
+	$sel1 delete
+}
+close $outfile
+quit
